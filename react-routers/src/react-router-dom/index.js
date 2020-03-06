3a439e8460a8e962286126d4db569f4074577621
hulongchao <hulc_it@163.com>
2020-03-06 21:57:37 +0800

    改变导出方式

--- react-routers/src/react-router-dom/index.js
+++ react-routers/src/react-router-dom/index.js
@@ -1,21 +1,31 @@
-import HashRouter from './HashRouter'
-import BrowserRouter from './BrowserRouter'
-import Route from './Route'
-import Link from './Link'
-import NavLink from './NavLink'
-import Switch from './Switch'
-import Redirect from './Redirect'
-import withRouter from './withRouter'
-import Prompt from './Prompt'
+// import HashRouter from './HashRouter'
+// import BrowserRouter from './BrowserRouter'
+// import Route from './Route'
+// import Link from './Link'
+// import NavLink from './NavLink'
+// import Switch from './Switch'
+// import Redirect from './Redirect'
+// import withRouter from './withRouter'
+// import Prompt from './Prompt'
 
-export {
-  HashRouter,
-  BrowserRouter,
-  Route,
-  Link,
-  NavLink,
-  Switch,
-  Redirect,
-  withRouter,
-  Prompt
-}
+// export {
+//   HashRouter,
+//   BrowserRouter,
+//   Route,
+//   Link,
+//   NavLink,
+//   Switch,
+//   Redirect,
+//   withRouter,
+//   Prompt
+// }
+
+export { default as HashRouter } from './HashRouter'
+export { default as BrowserRouter } from './BrowserRouter'
+export { default as Route } from './Route'
+export { default as Link } from './Link'
+export { default as NavLink } from './NavLink'
+export { default as Switch } from './Switch'
+export { default as Redirect } from './Redirect'
+export { default as withRouter } from './withRouter'
+export { default as Prompt } from './Prompt'
