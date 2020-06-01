ff9c40b60e6314f05fff372e6253a7f1bb49eba8
hulongchao <hulc_it@163.com>
2020-06-01 23:39:21 +0800

    useImperativeHandle

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, forwardRef, useImperativeHandle } from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 import './index.less'
 import avatar from '@/images/avatar.jpg'
 
+function Child(props, ref) {
+  const inputRef = useRef()
+  useImperativeHandle(ref, () => ({
+    focus() {
+      handleFocus()
+    },
+    greet() {
+      console.log('hello ')
+    }
+  }))
+  function handleFocus() {
+    inputRef.current.focus()
+  }
+  function handleBlur() {
+    inputRef.current.blur()
+  }
+  return (
+    <>
+      <p>我是子元素</p>
+      <button onClick={handleBlur}>失去焦点</button>
+      <input ref={inputRef} />
+    </>
+  )
+}
+
+Child = forwardRef(Child)
+
 function App() {
   const [count, setCount] = useState(0)
   const [value, setValue] = useState('')
   const [hobby, setHobby] = useState([])
+  const childRef = useRef()
 
   useEffect(() => {
     console.log('count', count)
@@ -41,8 +69,19 @@ function App() {
     setValue(e.target.value)
   }
 
+  function handleChildClick() {
+    childRef.current.focus()
+  }
+
+  function greet() {
+    childRef.current.greet()
+  }
+
   return (
     <div className='container'>
+      <button onClick={handleChildClick}>触发子元素的聚焦</button>
+      <button onClick={greet}>子元素打个招呼</button>
+      <Child ref={childRef} />
       <div className='empty'></div>
       <p>{ count }</p>
       <button onClick={fetchData}>获取数据</button>
