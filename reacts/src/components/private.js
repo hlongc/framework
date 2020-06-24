731509047a0949ba4b46f0556d28d4edee2baea2
hulongchao <hulc_it@163.com>
2020-06-24 09:56:56 +0800

    fixed <Private /> Component props Error

--- reacts/src/components/private.js
+++ reacts/src/components/private.js
@@ -4,7 +4,7 @@ import { Route, Redirect } from '../react-router-dom'
 export default class Private extends React.Component {
   render() {
     const username = sessionStorage.getItem('username')
-    const { to, component } = this.props
-    return username ? <Route to={to} children={component} /> : <Redirect to="/login" />
+    const { path, component } = this.props
+    return username ? <Route path={path} component={component} /> : <Redirect to="/login" />
   }
 }
