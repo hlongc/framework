4a550aad69c0f506f4d970b3600477ec9ca565d5
hulongchao <hulc_it@163.com>
2020-07-03 17:14:40 +0800

    抽离自定义hooks: 当前组件是否为初次渲染

--- reacts/src/index.js
+++ reacts/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router, Switch, Link, Route, Redirect } from 'react-router-dom'
 
@@ -16,7 +16,7 @@ function User(props) {
   return <div>User</div>
 }
 
-function App() {
+function App1() {
   return (
     <Router>
       <div>
@@ -31,6 +31,42 @@ function App() {
         <Redirect to='/home' />
       </Switch>
     </Router>
+  )
+}
+
+// 自定义hooks: 组件是否为更新渲染
+function useFirstRender() {
+  const firstRender = useRef(false)
+  useEffect(() => {
+    firstRender.current = true
+  }, [])
+  return firstRender.current
+}
+
+function Child() {
+  const firstRender = useFirstRender()
+  useEffect(() => {
+    if (!firstRender) { // 如果不是第一次渲染，代表更新操作
+      console.log('child update')
+    } else {
+      console.log('render')
+    }
+  })
+
+  return (
+    <div>我是子元素</div>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <p>{ count }</p>
+      <p>count 大于等于5 && 小于等于 10时隐藏子元素</p>
+      <button onClick={() => setCount(count + 1)}>count+</button>
+      { count < 5 || count > 10 ? <Child /> : null }
+    </div>
   )
 }
 
